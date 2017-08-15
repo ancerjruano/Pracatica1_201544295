89cac24435d234dbac7272b44ab924dbcac4b416
ancer <ancer@DESKTOP-TTQL13C>
2017-08-14 19:14:34 -0600

    creacion inicial del proyecto

---
+++ src/practica1/Practica1.java
@@ -0,0 +1,17 @@
+
+package practica1;
+
+/**
+ *
+ * @author ancer
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
